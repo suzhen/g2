5482ba18c9f6236fb629ddc2b44e5b6a7c9b648c
lizhe <mark.geek.lee@gmail.com>
2013-12-03 23:05:43 +0800

    wip

--- lib/g2/cli.rb
+++ lib/g2/cli.rb
@@ -2,10 +2,14 @@ require "thor"
 
 module G2
   class Cli < Thor
+    include Thor::Actions
 
     desc "new", "create a new grape goliath application"
-    def new app
-      say "create a new grape goliath application named #{app}", :green
+    def new app_root
+      say "create a new grape goliath application named #{app_root}", :green
+      %w{app config db}.each do |item|
+        empty_directory app_root + "/" + item
+      end
     end
     
   end
